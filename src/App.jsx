4d9b910a17c84a343b5bbf8b69b4147b7d7fdc5c
Matheus Foliatti <matheusfoliatti@gmail.com>
2023-03-31 21:06:54 -0300

    cover

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './global.css'
 export function App() {
   return(
     <section className="">
-      <h1>cover</h1>
+      <img src='https://images.unsplash.com/photo-1511512578047-dfb367046420?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=350&q=80' alt=''/>
       <h1>avatar</h1>
       <h1>Descrição/Nome</h1>
       <h1>Stack</h1>
